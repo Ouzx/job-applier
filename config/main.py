bb78ba48c8f3287ab359813a6bb504cab371aae5
Ouzx <oguzhankandakoglu@gmail.com>
2023-08-11 01:34:36 +0300

    selectors added

--- config/main.py
+++ config/main.py
@@ -1,4 +1,24 @@
 link = 'https://en.it-jobs.de/'
 driver_path = 'driver/chromedriver'
 
-selectors = {}
+selectors = {
+    # initial form
+    'place_text_box': '#plz-search-bar',
+    'tags_text_box': '#tag-search-input',
+    'expereince_buttons': '.btn-check:not(.languages .btn-check)',
+    'match_button': '#match-button',
+
+    # job list
+    'job_result': '.result',
+    'apply_button': '.btn-action',
+    'email_input': '.input-email input',
+    'email_name': '.input-name input',
+    'attach_resume_button': '.file-upload-button-canvas input',
+    'submit_button': '#send-application-btn',
+
+    # wait 1 second
+
+    # load more jobs
+    'more_jobs_button': '.load-more-btn',
+    'result_list_divider': 'results-page-divider-component',
+}
--- scraper/main.py
+++ scraper/main.py
@@ -1,8 +1,8 @@
-import time
+# import time
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
 
 from fake_useragent import UserAgent
 
